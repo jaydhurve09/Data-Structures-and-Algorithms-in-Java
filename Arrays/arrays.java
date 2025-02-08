ba4b06f6c47fc57a5b9ebde378c13608ec4bfed0
jaydhurve09 <jaydhurve09@gmail.com>
2025-02-08 19:44:38 +0530

    arrays.java created

---
+++ Arrays/arrays.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class arrays {
+public static void main(String[] args) {
+    int[] arr = new int[5];
+    //normally assinging array 
+    arr[0] = 15;                
+    arr[1] = 7;
+    arr[2] = 32;
+    arr[3] = 891;
+    arr[4] = 55;
+    System.out.println(arr[3]);     //printing a single element of an array ds
+}   
+}
