0cb3421dd79a939a581506fcb588e790496b55de
jaydhurve09 <jaydhurve09@gmail.com>
2025-03-02 20:56:57 +0530

    refined ceilingOfNumber problem

--- BinarySearchQuestions/ceilingOfNumber.java
+++ BinarySearchQuestions/ceilingOfNumber.java
@@ -7,20 +7,21 @@ package BinarySearchQuestions;
 class ceilingOfNumber {
     public static void main(String[] args) {
         int a[] = {2, 3, 5, 9, 14, 16, 18};
-        int res = search(a, 4);
+        int res = search(a, 15);
         System.out.println(res);
     }
     public static int search(int arr[], int target) {
         int start = 0;
         int end = arr.length - 1;
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end-start) / 2;
-            if(arr[mid] >= target) {
-                end = mid;  //if element is greater than or equal to target set end to mid, it could be the ans
+            if(arr[mid] == target) return arr[mid];
+            else if(arr[mid] < target) {
+                start = mid + 1;  //if element is less than to target set start to next of mid, it could be the ans
             } else {
-                start = mid + 1;
+                end = mid - 1;  //if element at mid is greater than target set end to element before mid
             }
         }
-        return arr[end];
+        return arr[start];  //start points to the ceiling
     }
 }
