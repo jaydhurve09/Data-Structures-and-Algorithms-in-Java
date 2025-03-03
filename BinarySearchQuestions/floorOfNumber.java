f364ca3f15df0b310f23d5134fff3c8d12208ad2
jaydhurve09 <jaydhurve09@gmail.com>
2025-03-03 11:58:12 +0530

    refined floorOfNumber

--- BinarySearchQuestions/floorOfNumber.java
+++ BinarySearchQuestions/floorOfNumber.java
@@ -9,21 +9,23 @@ public class floorOfNumber {
         floorOfNumber obj = new floorOfNumber();      //obj to access no-static methods in the class
         int a[] = {2, 3, 5, 9, 14, 16, 18};
         int res = obj.search(a, 10);            //search method called with arguments: array a, and target
-        System.out.println(res);
+        if(res==-1) System.out.println("Not found!");
+        else System.out.println(res);
     }
     public int search(int arr[], int target) {
         int start = 0;
         int end = arr.length - 1;
-        while(start<end){          //if we use <= then it might be in infinite loop
+        while(start<=end){          //we use <= to avoid infinite loop
             int mid = start + (end-start) / 2;
             if(arr[mid]==target) {      //found target at mid index
                 return arr[mid];
             } else if (arr[mid]<target) {
-                start = mid;        //we are moving target to mid not mid+1 because mid might be the answer
+                start = mid+1;        //we are moving start to mid+1
             } else {                //if arr[mid]>target then end will be previous element of mid
                 end = mid-1;
             }
         }
-        return arr[start];
+        return (end>=0) ? arr[end] : -1;        //returning end because it is largest of the smaller nos. than target
+        //check condition when no. not found and end becomes less than start return -1
     }
 }
