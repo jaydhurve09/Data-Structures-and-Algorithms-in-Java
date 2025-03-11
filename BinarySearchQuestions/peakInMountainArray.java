94a52beaefc71a162cfcf4329dad1513f802ca94
jaydhurve09 <jaydhurve09@gmail.com>
2025-03-11 18:45:29 +0530

    peakInMountainArray.java created

---
+++ BinarySearchQuestions/peakInMountainArray.java
@@ -0,0 +1,11 @@
+package BinarySearchQuestions;
+
+public class peakInMountainArray {
+    public static void main(String[] args) {
+
+    }
+    static int search(int[] arr) {
+
+        return -1;
+    }
+}
