c15af621ddd50ad2b087a878db869dafab5c8e18
jaydhurve09 <jaydhurve09@gmail.com>
2025-03-13 09:20:14 +0530

    peakInMountainArray.java updated

--- BinarySearchQuestions/peakInMountainArray.java
+++ BinarySearchQuestions/peakInMountainArray.java
@@ -2,10 +2,23 @@ package BinarySearchQuestions;
 
 public class peakInMountainArray {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 5, 7, 6, 3, 2};
+        int res = search(arr);
+        System.out.println("Peak index is: "+res);
     }
-    static int search(int[] arr) {
-
+    static int search(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<end) {
+            int mid = start + (end-start) / 2;
+            if (nums[mid]>nums[mid+1]){
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+            //when start and end are equal that means we've found the peak so return it.
+            return start;
+        }
         return -1;
     }
 }
