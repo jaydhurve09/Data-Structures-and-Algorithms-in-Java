496677e409c9fae893ef81e54f7502a17cd57422
jaydhurve09 <jaydhurve09@gmail.com>
2025-05-17 18:04:55 +0530

    PrintNum.java added

---
+++ Recursion/printNum.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class printNum {
+    public static void main(String[] args) {
+        int num = 5;
+        printN(num);
+    }
+
+    static void printN(int n) {
+        if (n==0) {
+            return;
+        }
+        System.out.print(n+" ");
+        printN(n-1);
+    }
+}
