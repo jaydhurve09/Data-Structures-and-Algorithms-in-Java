ed55c4171bfe8173fbb6e8bb332306867097b294
jaydhurve09 <jaydhurve09@gmail.com>
2025-05-17 10:30:15 +0530

    comment added to Test.java

--- Trees/Test.java
+++ Trees/Test.java
@@ -31,6 +31,7 @@ public class Test {
     }
 
     public static void main(String[] args) {
+        //preorder
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
