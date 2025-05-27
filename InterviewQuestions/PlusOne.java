04aab8928ec444b45236a38c5678354c72f81eb0
jaydhurve09 <jaydhurve09@gmail.com>
2025-05-27 11:33:35 +0530

    Comments added in PlusOne.java

--- InterviewQuestions/PlusOne.java
+++ InterviewQuestions/PlusOne.java
@@ -2,6 +2,12 @@ package InterviewQuestions;
 
 import java.util.Arrays;
 
+//given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order.
+//The large integer does not contain any leading 0's.
+//Increment the large integer by one and return the resulting array of digits.
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
 public class PlusOne {
     public static void main(String[] args) {
         int[] arr = {9};
@@ -18,6 +24,7 @@ public class PlusOne {
             digits[i] = 0;
         }
 
+        //if all digits were 9
         int[] newNum = new int[n+1];
         newNum[0]=1;
         return newNum;
