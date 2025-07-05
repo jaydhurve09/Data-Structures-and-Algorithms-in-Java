8b41e92cd261affeca542f888d748c5d27a59cd9
jaydhurve09 <jaydhurve09@gmail.com>
2025-07-05 11:06:49 +0530

    Length of the Linked List Cycle

--- LinkedList/Questions/CycleInLL.java
+++ LinkedList/Questions/CycleInLL.java
@@ -6,6 +6,7 @@ public class CycleInLL {
     List list = new List();
 
     //fast and slow approach
+    //Detect the cycle
     public static boolean isCycle(List head) {
         List fast = head;
         List slow = head;
@@ -18,5 +19,65 @@ public class CycleInLL {
         }
 
         return false;
+    }
+
+
+    // Length of the cycle
+    public static int LengthCycle(List head) {
+        List fast = head;
+        List slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                List temp = slow;
+                int length = 0;
+                do{
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+
+    public List detectCycle(List head) {
+        int length = 0;
+
+        List fast = head;
+        List slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = LengthCycle(slow);
+                break;
+            }
+        }
+
+        //if no cycle present
+        if(length == 0) {
+            return null;
+        }
+
+        //find the start
+        List s = head;
+        List f = head;
+
+        while(length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        // keep moving till both meet at start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
     }
 }
